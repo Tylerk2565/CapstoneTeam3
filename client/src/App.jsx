8876232b4d8d22fc1c14396fc8abe87069d29566
‘SkyPB’ <skyshanice01@gmail.com>
2025-03-12 15:31:35 -0400

    create home page and quiz component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -6,17 +8,13 @@ import QuizPage from "../src/pages/QuizPage";
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/quiz" element={<QuizPage />} />
-          </Routes>
-        </div>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
