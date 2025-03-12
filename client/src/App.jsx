66a68b51e754d9e7deb81df2164955e8a07ab49f
‘SkyPB’ <skyshanice01@gmail.com>
2025-03-12 15:37:18 -0400

    adjust app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import QuizPage from "../src/pages/QuizPage";
@@ -9,11 +8,17 @@ import QuizPage from "../src/pages/QuizPage";
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
     </>
   );
 };
